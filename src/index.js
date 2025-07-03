bc53573642c6e6e076d9539f4e8af4f0b3045d00
DangHung1110 <hung121nkd@gmail.com>
2025-07-03 12:25:07 +0700

    test

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT
 instanceMongoDB.connect()
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: 'http://localhost:5173',
   credentials: true
 }))
 app.use(express.json())
